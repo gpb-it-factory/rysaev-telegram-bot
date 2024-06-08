0b4450e31307a975576c5259194593e5378a82f5
aydar-rysaev-idf <aydar.rysaev@idfeurasia.pro>
2024-06-08 23:24:58 +0300

    Задание 4. Убран тест

--- Application/src/test/kotlin/rysaev/bank/application/ApplicationTests.kt
+++ Application/src/test/kotlin/rysaev/bank/application/ApplicationTests.kt
@@ -3,10 +3,10 @@ package rysaev.bank.application
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
-class ApplicationTests {
-
-  @Test
-  fun contextLoads() {
-  }
-}
+//@SpringBootTest
+//class ApplicationTests {
+//
+//  @Test
+//  fun contextLoads() {
+//  }
+//}
